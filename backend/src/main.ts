265ca80fc172512633c23be0cbef17f56830e609
Swera Tanveer <sweratanveer05@gmail.com>
2026-07-13 17:07:11 +0500

    Remove backend files from frontend branch

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,10 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:4200',
-      'http://localhost:4201',
-    ],
+    origin: 'http://localhost:4200',
     credentials: true,
   });
 
