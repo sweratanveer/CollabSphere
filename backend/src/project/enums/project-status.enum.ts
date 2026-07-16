b650a23584d039f8c79f36bdf3e6bf9488d6abde
Swera Tanveer <sweratanveer05@gmail.com>
2026-07-16 16:44:07 +0500

    deleted files

--- backend/src/project/enums/project-status.enum.ts
+++
@@ -1,8 +0,0 @@
-// This file defines the workflow status stages a project can be in.
-export enum ProjectStatus {
-  PLANNING = 'PLANNING',
-  IN_PROGRESS = 'IN_PROGRESS',
-  ON_HOLD = 'ON_HOLD',
-  COMPLETED = 'COMPLETED',
-  CANCELLED = 'CANCELLED',
-}
